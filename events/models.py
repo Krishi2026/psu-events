29ac96a0b71ea0a3dac84bcb234fa594fb3f36e3
sainutheti <146263097+sainutheti@users.noreply.github.com>
2023-11-01 07:48:00 +0530

    Adding Events Page

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Model for the Venue Table
@@ -6,9 +7,9 @@ class Venue(models.Model):
 	name = models.CharField('Venue Name', max_length=120)
 	address = models.CharField(max_length=300)
 	zip_code = models.CharField('Zip Code', max_length=15)
-	phone = models.CharField('Contact Number', max_length=15)
-	web = models.URLField('Website Address')
-	email_address = models.EmailField('Email')
+	phone = models.CharField('Contact Number', max_length=15, blank=True)
+	web = models.URLField('Website Address',blank=True)
+	email_address = models.EmailField('Email Address',blank=True)
 
 	def __str__(self):
 		return self.name
@@ -31,7 +32,7 @@ class Event(models.Model):
 	name = models.CharField('Event Name', max_length=120)
 	event_date = models.DateTimeField('Event Date', )
 	venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-	manager =  models.CharField(max_length=60)
+	manager =  models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 	description = models.TextField(blank=True)
 	attendees = models.ManyToManyField(Members, blank=True)
 
