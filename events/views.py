308338f7ce1b7ac63106ec6e13885be38515d634
Hanesh <hanesh@pdx.edu>
2023-11-01 15:52:17 -0700

    made changes to the admin area

--- events/views.py
+++ events/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-def home(request, year, month):
-	name = "Hanesh"
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm
+
+
+
+
+
+def all_events(request):
+	event_list = Event.objects.all()
+	return render(request, 'events/event_list.html',
+		{"event_list": event_list})
+
+
+
+
+
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	#Converting month to uppercase
 	month = month.capitalize()
 	# Month Conversation
@@ -18,8 +36,7 @@ def home(request, year, month):
 	#Current Time and Year
 	current_time = datetime.now()
 	current_year = current_time.year
-	return render(request,'home.html',{
-	"name": name,
+	return render(request,'events/home.html',{
 	"year": year,
 	"month": month,
 	"month_number":month_number,
