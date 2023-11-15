fdbfa0a5602eb4a01c5513ca1bc6272f4175f26e
sainutheti <146263097+sainutheti@users.noreply.github.com>
2023-11-16 05:17:33 +0530

    Add the Events adding page

--- events/views.py
+++ events/views.py
@@ -2,8 +2,82 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-def home(request, year, month):
-	name = "Hanesh"
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm, EventForm
+from django.shortcuts import redirect
+
+
+def show_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	return render(request, 'events/show_venue.html',
+		{'venue':venue})
+
+
+def update_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	form = VenueForm(request.POST or None, instance=venue)
+	if form.is_valid():
+		form.save()
+		return redirect('list-venues')
+	return render(request, 'events/update_venue.html',
+		{'venue':venue, 
+   		'form':form})
+
+def all_events(request):
+	event_list = Event.objects.all()
+	return render(request, 'events/event_list.html',
+		{"event_list": event_list})
+
+def add_event(request):
+	submitted = False
+	if request.method == "POST":
+		form = EventForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_event?submitted=True')
+	else:
+		form = EventForm
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 'events/add_event.html',
+		{'form':form,'submitted':submitted})
+
+
+def list_venues(request):
+	venue_list = Venue.objects.all()
+	return render(request, 'events/venue.html',
+		{'venue_list':venue_list})
+
+
+def search_venues(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		venues = Venue.objects.filter(name__contains=searched)
+
+		return render(request, 'events/search_venues.html',
+		{'searched':searched,'venues':venues})
+	else:
+		return render(request, 'events/search_venues.html',
+		{})
+
+def add_venue(request):
+	submitted = False
+	if request.method == "POST":
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_venue?submitted=True')
+
+	else:
+		form = VenueForm
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 'events/add_venue.html',
+		{'form':form,'submitted':submitted})
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	#Converting month to uppercase
 	month = month.capitalize()
 	# Month Conversation
